7b0b85aef68ece530cafca79366ae2b309faeec1
Linnéa Irbladh <56582158+linneairbladh@users.noreply.github.com>
2023-09-28 11:09:05 +0200

    fix: add googe map packages

--- vite.config.ts
+++ vite.config.ts
@@ -2,8 +2,8 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import mdPlugin, { Mode } from "vite-plugin-markdown";
 // https://vitejs.dev/config/
+
 export default defineConfig({
   plugins: [react(), mdPlugin({ mode: [Mode.REACT] })],
-  envDir: "./env/",
   base: "",
 });
