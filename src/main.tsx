f48b16248fed5e1e74f77f4f058d0d8edd33a41e
Linnéa Irbladh <56582158+linneairbladh@users.noreply.github.com>
2023-10-02 17:06:56 +0200

    refactor: change to hash router

--- src/main.tsx
+++ src/main.tsx
@@ -3,12 +3,12 @@ import ReactDOM from "react-dom/client";
 import App from "./App";
 import "./styles/index.css";
 import "./styles/markdown.scss";
-import { BrowserRouter } from "react-router-dom";
+import { HashRouter } from "react-router-dom";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <BrowserRouter>
+    <HashRouter>
       <App />
-    </BrowserRouter>
+    </HashRouter>
   </React.StrictMode>
 );
